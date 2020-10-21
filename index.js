6267d03bea7577c61601ebca549380b35604aaaf
Dan Teodora-Alexandra <teodora_dan@yahoo.com>
2020-10-21 13:36:08 +0300

    Write some code for proper debugging

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log("Hello world!");
+
+// eslint-disable-next-line no-undef
+const chalk = require("chalk");
+console.log(chalk.blue("Hello world!"));
+
+// eslint-disable-next-line no-undef
+const ProgressBar = require("progress");
+
+const bar = new ProgressBar(":bar", { total: 10 });
+const timer = setInterval(() => {
+  bar.tick();
+  if (bar.complete) {
+    clearInterval(timer);
+  }
+}, 100);
+
+// eslint-disable-next-line no-undef
+const readline = require("readline").createInterface({
+  // eslint-disable-next-line no-undef
+  input: process.stdin,
+  // eslint-disable-next-line no-undef
+  output: process.stdout
+});
+
+readline.question("What's your name?", name => {
+  console.log(`Hi ${name}!`);
+  readline.close();
+});
