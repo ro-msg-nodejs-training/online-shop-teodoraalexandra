9b30ca8e4bac88730e433701d0c1943636e651da
Dan Teodora-Alexandra <teodora_dan@yahoo.com>
2020-10-27 10:13:26 +0200

    Replace products endpoints with mongoose

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -11,21 +11,71 @@ exports.product_list = function(req, res) {
 };
 
 // eslint-disable-next-line no-undef
+exports.product_list_categories = function(req, res) {
+  Product.find({ "category" : req.params.cid }, function (err, list_products_categories) {
+    // If object found return an object else return 404 not-found
+    if (err) { res.sendStatus(404); }
+    res.status(200).json(list_products_categories);
+  });
+};
+
+// eslint-disable-next-line no-undef
 exports.product_detail = function(req, res) {
-  res.send("NOT IMPLEMENTED: Product detail: " + req.params.id);
+  Product.find({ "id" : req.params.id }, function (err, product) {
+    // If object found return an object else return 404 not-found
+    if (err) { res.sendStatus(404); }
+    res.status(200).json(product);
+  });
 };
 
 // eslint-disable-next-line no-undef
 exports.product_create = function(req, res) {
-  res.send("NOT IMPLEMENTED: Product create");
+  // Create an object of new Item
+  let newProduct = {
+    id: req.body.id,
+    name: req.body.name,
+    description: req.body.description,
+    price: req.body.price,
+    weight: req.body.weight,
+    category: parseInt(req.params.cid),
+    imageUrl: req.body.imageUrl
+  };
+
+  // Push new item object to database
+  Product.create(newProduct, function (err, newItem) {
+    if (err) { res.sendStatus(404); }
+    // Return with status 201 - created
+    res.status(201).json(newItem);
+  });
 };
 
 // eslint-disable-next-line no-undef
 exports.product_update = function(req, res) {
-  res.send("NOT IMPLEMENTED: Product update");
+  let updated = {
+    id: req.params.id,
+    name: req.body.name,
+    description: req.body.description,
+    price: req.body.price,
+    weight: req.body.weight,
+    category: req.body.category,
+    imageUrl: req.body.imageUrl
+  };
+
+  // Get item object match by `id`
+  Product.findOneAndUpdate({ "id" : req.params.id }, updated,function (err) {
+    // If object found return an object else return 404 not-found
+    if (err) { res.sendStatus(404); }
+
+    res.status(201).json(updated);
+  });
 };
 
 // eslint-disable-next-line no-undef
 exports.product_delete = function(req, res) {
-  res.send("NOT IMPLEMENTED: Product delete");
+  Product.deleteOne({ "id" : req.params.id }, function (err) {
+    // If object found return an object else return 404 not-found
+    if (err) { res.sendStatus(404); }
+
+    res.sendStatus(204);
+  });
 };
--- routes/products.js
+++ routes/products.js
@@ -7,8 +7,9 @@ const router = express.Router();
 // eslint-disable-next-line no-undef
 const products_controller = require("../controllers/productsController");
 router.get("/", products_controller.product_list);
+router.get("/categories/:cid", products_controller.product_list_categories);
 router.get("/:id", products_controller.product_detail);
-router.post("/", products_controller.product_create);
+router.post("/categories/:cid", products_controller.product_create);
 router.put("/:id", products_controller.product_update);
 router.delete("/:id", products_controller.product_delete);
 
